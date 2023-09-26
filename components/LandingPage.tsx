215fd7a9911a72fe718bd2f176dd5e9ad7f87efa
jacklion710 <jack.lion710@gmail.com>
2023-09-26 19:50:31 -0400

    Changed text colors and added glow

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -27,12 +27,13 @@ const LandingPage = () => {
       </Helmet>
       <Box bgColor="rgba(230, 230, 230, 0.7)" p={6} borderRadius="md">
         <VStack spacing={6}>
-          <Heading 
+        <Heading 
             fontSize={['2xl', '3xl', '4xl', '5xl']} 
-            color="teal.500"
-          >
+            color="white"
+            textShadow="0 0 5px teal, 0 0 10px teal, 0 0 15px teal, 0 0 20px teal"
+        >
             Jacob Leone
-          </Heading>
+        </Heading>
           <VStack spacing={2}>
             <AiFillRobot size="1.5em" color="gray.700" />
             <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">AI Specialist</Text>
@@ -50,14 +51,14 @@ const LandingPage = () => {
               Discover My Innovations
           </Button>
           <Box textAlign="center" w="100%" my={4}>
-            <Text fontSize="lg" color="teal.500" mb={1} maxW="700px" textAlign="center" mx="auto">
+            <Text fontSize="lg" color="black" mb={1} maxW="700px" textAlign="center" mx="auto">
               I am driven by an intrinsic passion for tackling both creative and technical challenges. As part of agile development teams, I thrive on collaborating towards our shared mission: delivering exceptional value to clients efficiently and effectively...
             </Text>
             <Button variant="link" color="teal.500" onClick={onToggle} mt={2}>
               {isOpen ? "Read Less" : "Read More"} {isOpen ? <FaAngleUp /> : <FaAngleDown />}
             </Button>
             <Collapse in={isOpen}>
-              <Text fontSize="lg" color="teal.500" mt={4} maxW="700px" textAlign="center" mx="auto">
+              <Text fontSize="lg" color="black" mt={4} maxW="700px" textAlign="center" mx="auto">
                 Before diving into the tech arena, I pursued an intensive graduate program at a top-tier university, focusing on the convergence of artificial intelligence and psychology. This academic journey endowed me with a unique lens: approaching AI development from a human-centric perspective and deeply understanding the nuanced ways people interact with technology. Beyond my academic pursuits, I am a perpetual learner, drawing from experiences that span leadership, creativity, problem-solving, and shepherding projects from mere ideas to successful execution.
               </Text>
             </Collapse>
