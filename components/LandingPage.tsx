1667ea45845d220778fc86e5f92fc4108de76d33
jacklion710 <jack.lion710@gmail.com>
2023-09-25 23:06:04 -0400

    Added a background image

--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -1,12 +1,39 @@
-import { Heading, Text, VStack, Center } from "@chakra-ui/react";
+import { Heading, Text, VStack, Center, Button, Box } from "@chakra-ui/react";
 
 const LandingPage = () => {
   return (
-    <Center height="85vh" bg="gray.50">
-      <VStack spacing={6}>
-        <Heading>Jacob Leone</Heading>
-        <Text>AI Specialist & Software Engineer.</Text>
-      </VStack>
+    <Center 
+      height="88vh" 
+      bg="gray.50" 
+      bgImage="url('/images/circuit.jpeg')"
+      bgPos="center"
+      bgSize="cover"
+      bgRepeat="no-repeat"
+    >
+      <Box bgColor="rgba(230, 230, 230, 0.7)" p={6} borderRadius="md">
+        <VStack spacing={6}>
+          <Heading 
+            fontSize={['2xl', '3xl', '4xl', '5xl']} 
+            color="teal.500"
+          >
+            Jacob Leone
+          </Heading>
+          <Text 
+            fontSize={['lg', 'xl', '2xl']} 
+            color="gray.700"
+          >
+            AI Specialist & Software Engineer.
+          </Text>
+          <Button 
+              colorScheme="teal" 
+              size="lg" 
+              variant="outline" 
+              onClick={() => window.location.href="/portfolio"}
+          >
+              View My Work
+          </Button>
+        </VStack>
+      </Box>
     </Center>
   );
 };
