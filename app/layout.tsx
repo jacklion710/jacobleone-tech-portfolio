be65bf9a53bbb532a0ee69a9bcf306827f2c48de
jacklion710 <jack.lion710@gmail.com>
2023-11-02 22:57:47 -0400

    Implemented intercept observer and framer motions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,22 +2,44 @@
 import { ChakraProvider, Box } from "@chakra-ui/react"
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+
+  const [navbarRef, navbarInView] = useInView({
+    triggerOnce: true, 
+    threshold: 0.1, 
+  });
+  
+  const [footerRef, footerInView] = useInView({
+    triggerOnce: true, 
+    threshold: 0.9, 
+  });
+  
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1.33 } }
+  };
+
   return (
     <html lang="en">
       <body>
         <ChakraProvider>
           <Box display="flex" flexDirection="column" minHeight="100vh">
-            <Navbar />
-            <Box flex="1"> 
+            <motion.div ref={navbarRef} initial="hidden" animate={navbarInView ? "visible" : "hidden"} variants={fadeIn}>
+              <Navbar />
+            </motion.div>            
+          <Box flex="1"> 
                 {children}
-            </Box>
-            <Footer />
+          </Box>
+            <motion.div ref={footerRef} initial="hidden" animate={footerInView ? "visible" : "hidden"} variants={fadeIn}>
+              <Footer />
+            </motion.div>
           </Box>
         </ChakraProvider>
       </body>
--- components/LandingPage.tsx
+++ components/LandingPage.tsx
@@ -4,10 +4,22 @@ import { FaCode, FaAngleDown, FaAngleUp } from 'react-icons/fa';
 import { Helmet } from "react-helmet";
 import Head from 'next/head';
 import Image from 'next/image';
+import { useInView } from 'react-intersection-observer';
+import { motion } from 'framer-motion';
 
 const LandingPage = () => {
   const { isOpen, onToggle } = useDisclosure();
-  const imageSize = useBreakpointValue({ base: 230, sm: 120, md: 200, lg: 310, xl: 180 });
+  const imageSize = useBreakpointValue({ base: 350, sm: 400, md: 450, lg: 450, xl: 450 });
+
+  const [imageRef, imageInView] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [iconsRef, iconsInView] = useInView({ triggerOnce: true, threshold: 0.3 });
+  const [buttonRef, buttonInView] = useInView({ triggerOnce: true, threshold: 0.8 });
+  const [textRef, textView] = useInView({ triggerOnce: true, threshold: 0.5 });
+
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 2 } }
+  };
 
   return (
     <Center 
@@ -35,44 +47,45 @@ const LandingPage = () => {
       </Helmet>
       <Box bgColor="rgba(230, 230, 230, 0.7)" p={6} borderRadius="md">
         <VStack spacing={6}>
-        <Image 
-            src="/vectors/Tech_Typography_Vectorized(white).svg" 
-            alt="Jacob Leone" 
-            height={imageSize} 
-            width={imageSize} 
-            style={{
-                filter: "drop-shadow(0 0 3px teal) drop-shadow(0 0 6px teal)"
-            }}
-          />
-          <VStack spacing={2}>
-            <AiFillRobot size="1.5em" color="gray.700" />
-            <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">AI Specialist</Text>
-          </VStack>
-          <VStack spacing={2}>
-            <FaCode size="1.5em" color="gray.700" />
-            <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">Software Engineer</Text>
-          </VStack>
-          <Button 
-              colorScheme="teal" 
-              size="lg" 
-              variant="outline" 
-              onClick={() => window.location.href="/Projects"}
-          >
-              Discover My Innovations
-          </Button>
-          <Box textAlign="center" w="100%" my={4}>
-            <Text fontSize="lg" color="black" mb={1} maxW="700px" textAlign="center" mx="auto">
-              Driven by an intrinsic passion for tackling creative and technical challenges and as member of agile development teams, I thrive on collaboration towards a shared mission: delivering exceptional value to clients efficiently...
-            </Text>
-            <Button variant="link" color="teal.500" onClick={onToggle} mt={2}>
-              {isOpen ? "Read Less" : "Read More"} {isOpen ? <FaAngleUp /> : <FaAngleDown />}
-            </Button>
-            <Collapse in={isOpen}>
-              <Text fontSize="lg" color="black" mt={4} maxW="700px" textAlign="center" mx="auto">
-                Before diving into the tech arena, I pursued an intensive graduate program at a top-tier university, focusing on the convergence of artificial intelligence and psychology. This academic journey endowed me with a unique lens: approaching AI development from a human first perspective and a deep understanding of the nuanced ways people interface with machines. Beyond my academic pursuits, I am a perpetual learner, drawing from experiences that span leadership, creativity, problem-solving, and architecting projects from mere ideas to successful execution.
+          <motion.div ref={imageRef} initial="hidden" animate={imageInView ? "visible" : "hidden"} variants={fadeIn}>
+            <Image 
+              src="/vectors/Tech_Typography_Vectorized(white).svg" 
+              alt="Jacob Leone" 
+              height={imageSize} 
+              width={imageSize} 
+              style={{ filter: "drop-shadow(0 0 3px teal) drop-shadow(0 0 6px teal)" }}
+            />
+          </motion.div>
+            <VStack spacing={2} mt={10}>
+              <motion.div ref={iconsRef} initial="hidden" animate={iconsInView ? "visible" : "hidden"} variants={fadeIn}>
+                <VStack spacing={2}>
+                  <AiFillRobot size="1.5em" color="gray.700" />
+                  <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">AI Specialist</Text>
+                  <FaCode size="1.5em" color="gray.700" />
+                  <Text fontSize={['lg', 'xl', '2xl']} color="gray.700">Software Engineer</Text>
+                </VStack>
+              </motion.div>
+            </VStack>
+            <motion.div ref={buttonRef} initial="hidden" animate={buttonInView ? "visible" : "hidden"} variants={fadeIn}>
+              <Button colorScheme="teal" size="lg" variant="outline" onClick={() => window.location.href="/Projects"}>
+                Discover My Innovations
+              </Button>
+            </motion.div>
+            <motion.div ref={textRef} initial="hidden" animate={textView ? "visible" : "hidden"} variants={fadeIn}>
+            <Box textAlign="center" w="100%" my={4}>
+              <Text fontSize="lg" color="black" mb={1} maxW="700px" textAlign="center" mx="auto">
+                Driven by an intrinsic passion for tackling creative and technical challenges and as member of agile development teams, I thrive on collaboration towards a shared mission: delivering exceptional value to clients efficiently...
               </Text>
-            </Collapse>
-          </Box>
+              <Button variant="link" color="teal.500" onClick={onToggle} mt={2}>
+                {isOpen ? "Read Less" : "Read More"} {isOpen ? <FaAngleUp /> : <FaAngleDown />}
+              </Button>
+              <Collapse in={isOpen}>
+                <Text fontSize="lg" color="black" mt={4} maxW="700px" textAlign="center" mx="auto">
+                  Before diving into the tech arena, I pursued an intensive graduate program at a top-tier university, focusing on the convergence of artificial intelligence and psychology. This academic journey endowed me with a unique lens: approaching AI development from a human first perspective and a deep understanding of the nuanced ways people interface with machines. Beyond my academic pursuits, I am a perpetual learner, drawing from experiences that span leadership, creativity, problem-solving, and architecting projects from mere ideas to successful execution.
+                </Text>
+              </Collapse>
+            </Box>
+          </motion.div>
         </VStack>
       </Box>
     </Center>
