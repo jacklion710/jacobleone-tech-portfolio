24afe39919e5968cd392d6047fe5467173560d75
jacklion710 <jack.lion710@gmail.com>
2023-11-03 00:04:32 -0400

    Intercept-framer implementation

--- components/ReferencesComponent.tsx
+++ components/ReferencesComponent.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
-import {
-  Box,
-  Heading,
-  Text,
-  Link,
-  Flex
-} from '@chakra-ui/react';
+import { Box, Heading, Text, Link, Flex } from '@chakra-ui/react';
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
 
 const ReferencesComponent: React.FC = () => {
+  const fadeIn = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, transition: { duration: 1.33 } }
+  };
+
+  const [ref1, inView1] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref2, inView2] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [ref3, inView3] = useInView({ triggerOnce: true, threshold: 0.1 });
+  const [refAbout, inViewAbout] = useInView({ triggerOnce: true, threshold: 0.1 });
+
   return (
     <Box
       border="1px"
@@ -31,28 +37,42 @@ const ReferencesComponent: React.FC = () => {
         mt={5}
         w="full"
       >
-        <Heading color="white" fontSize="2xl" mb={2}>Borko Fuhrt</Heading>
-        <Text color="white" mb={1}>Professor and Director</Text>
-        <Text color="white" mb={1}>NSF Industry / University Cooperative Research Center</Text>
-        <Link href="mailto: bfurht@fau.edu" color="white" mb={1}>bfurht@fau.edu</Link>
-        <Link href="tel:5612973180" color="white" mb={1}>561-297-3180</Link>
-        <Text color="white">Relationship: Professor | Mentor</Text>
-        
-        <Heading color="white" fontSize="2xl" mb={2} mt={10}>Larry Canipe</Heading>
-        <Text color="white" mb={1}>CEO</Text>
-        <Text color="white" mb={1}>Vector Climate / Cleva Tech</Text>
-        <Link href="mailto: larrycanipe@vectorclimate.com" color="white" mb={1}>larrycanipe@vectorclimate.com</Link>
-        <Link href="tel:5616545279" color="white" mb={1}>561-654-5279</Link>
-        <Text color="white">Relationship: Former Employer | Mentor</Text>
+        <motion.div ref={ref1} initial="hidden" animate={inView1 ? "visible" : "hidden"} variants={fadeIn}>
+          <Flex direction="column" alignItems="center">
+            <Heading color="white" fontSize="2xl" mb={2}>Borko Fuhrt</Heading>
+            <Text color="white" mb={1}>Professor and Director</Text>
+            <Text color="white" mb={1}>NSF Industry / University Cooperative Research Center</Text>
+            <Link href="mailto: bfurht@fau.edu" color="white" mb={1}>bfurht@fau.edu</Link>
+            <Link href="tel:5612973180" color="white" mb={1}>561-297-3180</Link>
+            <Text color="white">Relationship: Professor | Mentor</Text>
+          </Flex>
+        </motion.div>
+          
+        <motion.div ref={ref2} initial="hidden" animate={inView1 ? "visible" : "hidden"} variants={fadeIn}>
+          <Flex direction="column" alignItems="center">
+            <Heading color="white" fontSize="2xl" mb={2} mt={10}>Larry Canipe</Heading>
+            <Text color="white" mb={1}>CEO</Text>
+            <Text color="white" mb={1}>Vector Climate / Cleva Tech</Text>
+            <Link href="mailto: larrycanipe@vectorclimate.com" color="white" mb={1}>larrycanipe@vectorclimate.com</Link>
+            <Link href="tel:5616545279" color="white" mb={1}>561-654-5279</Link>
+            <Text color="white">Relationship: Former Employer | Mentor</Text>
+          </Flex>
+        </motion.div>
+          
+        <motion.div ref={ref3} initial="hidden" animate={inView1 ? "visible" : "hidden"} variants={fadeIn}>
+          <Flex direction="column" alignItems="center">
+            <Heading color="white" fontSize="2xl" mb={2} mt={10}>Devin Nakano</Heading>
+            <Text color="white" mb={1}>Founder, President and Executive Director</Text>
+            <Text color="white" mb={1}>Y Stem and Chess</Text>
+            <Link href="mailto: devin@ystemandchess.com" color="white" mb={1}>devin@ystemandchess.com</Link>
+            <Text color="white">Relationship: Business Leader | Client</Text>
+          </Flex>
+        </motion.div>
 
-        <Heading color="white" fontSize="2xl" mb={2} mt={10}>Devin Nakano</Heading>
-        <Text color="white" mb={1}>Founder, President and Executive Director</Text>
-        <Text color="white" mb={1}>Y Stem and Chess</Text>
-        <Link href="mailto: devin@ystemandchess.com" color="white" mb={1}>devin@ystemandchess.com</Link>
-        <Text color="white">Relationship: Business Leader | Client</Text>
       </Flex>
 
-        <Heading textAlign="center" color="white" fontSize="2xl" mb={10} mt={10}>
+      <motion.div ref={refAbout} initial="hidden" animate={inViewAbout ? "visible" : "hidden"} variants={fadeIn}>
+        <Heading textAlign="center" color="white" fontSize="2xl" mb={10} mt={5}>
             ...
         </Heading>
 
@@ -60,6 +80,7 @@ const ReferencesComponent: React.FC = () => {
             About me
         </Heading>
         <Text color="white" textAlign="center" mb={1}>I am driven by an intrinsic passion for tackling both creative and technical challenges. As part of agile development teams, I thrive on collaborating towards our shared mission: delivering exceptional value to clients efficiently and effectively. Before diving into the tech arena, I pursued an intensive graduate program at a top-tier university, focusing on the convergence of artificial intelligence and psychology. This academic journey endowed me with a unique lens: approaching AI development from a human-centric perspective and deeply understanding the nuanced ways people interact with technology. Beyond my academic pursuits, I am a perpetual learner, drawing from experiences that span leadership, creativity, problem-solving, and shepherding projects from mere ideas to successful execution.</Text>
+      </motion.div>
 
     </Box>
   );
